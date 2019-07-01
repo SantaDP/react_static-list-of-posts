9e46cdeb46d2a90709cc9791cfa2a0b6c9b28307
Aleksandr Pereleev <aleksandr.pereleev@gmail.com>
2019-07-01 12:30:55 +0300

    Updates

---
+++ precache-manifest.56fd933159063cef4ad2f26944467197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b41c1d040622414ab0088164975e20",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "8565fcab85cedf9685f0",
+    "url": "/myapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fc1a04f5442fc70ce71f",
+    "url": "/myapp/static/js/2.f9075ab4.chunk.js"
+  },
+  {
+    "revision": "8565fcab85cedf9685f0",
+    "url": "/myapp/static/js/main.915de202.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
